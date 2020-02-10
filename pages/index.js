3dbd9be07cea4cda5bcae3208a2563be20b0b827
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-02-10 16:23:04 +0800

    learn about cookie auth in nextjs

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout";
+import Link from "next/link";
+import {authInitialProps} from "../lib/auth";
+
+export default function Index(props) {
+    return (
+        <Layout title={"Home"} {...props}>
+            <Link href={"/profile"}><a>Go to profile</a></Link>
+        </Layout>
+    )
+}
+
+Index.getInitialProps = authInitialProps();
